33e3499b39488c53708aaee02a3d2a682f30c6e2
Clarityform <momentshot4@gmail.com>
2025-10-02 12:39:51 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ set -o errexit
 # Optional: echo commands for debugging
 # set -o xtrace
 
+# Clone riffusion if it's not already in your repo
+if [ ! -d "riffusion" ]; then
+  git clone https://github.com/hmartiro/riffusion.git
+fi
+
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# (Optional) If you need to install other tools, do it here.
-# e.g., if Riffusion needs FFmpeg or other system deps, install here.
